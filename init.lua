b72ce7a889adf80764ab6fd8a054b0df404a316a
snehilshah <snehilshah27@gmail.com>
2025-07-19 22:34:19 +0530

    initial highlight on yank commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+require("config.lazy")
+print("advent of neovim")
+print("lmao")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+print("lmao you suck")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
