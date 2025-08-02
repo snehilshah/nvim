d44daa4a035c4114d0080d887d4c5bafc7e4a467
snehilshah <snehilshah27@gmail.com>
2025-08-03 03:03:54 +0530

    lua lsp plugin setup

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,19 +1,33 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
-    config = function()
-      require("lspconfig").lua_ls.setup {}
-    end,
-}
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local c = vim.lsp.get_client_by_id(args.data.client_id)
+					if not c then return end
+
+					vim.api.nvim_create_autocmd('BufWritePre', {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+						end,
+					})
+				end,
+			})
+		end,
+	}
 }
