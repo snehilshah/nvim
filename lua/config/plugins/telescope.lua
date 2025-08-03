e240ece828800f45919418d02b01adab665e4c97
snehilshah <snehilshah27@gmail.com>
2025-08-03 08:00:31 +0530

    telescope builtin defination

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
 	},
 	config = function()
+		local builtin = require('telescope.builtin')
 		require('telescope').setup {
 			defaults = require('telescope.themes').get_ivy(),
 			extensions = {
@@ -16,29 +17,29 @@ return {
 		require('telescope').load_extension('fzf')
 
 		-- GIT
-		vim.keymap.set("n", "<space>gc", require('telescope.builtin').git_commits)
-		vim.keymap.set("n", "<space>gb", require('telescope.builtin').git_branches)
-		vim.keymap.set("n", "<space>gss", require('telescope.builtin').git_status)
-		vim.keymap.set("n", "<space>gst", require('telescope.builtin').git_stash)
+		vim.keymap.set("n", "<space>gc", builtin.git_commits)
+		vim.keymap.set("n", "<space>gb", builtin.git_branches)
+		vim.keymap.set("n", "<space>gss", builtin.git_status)
+		vim.keymap.set("n", "<space>gst", builtin.git_stash)
 		-- FILES
-		vim.keymap.set("n", "<space>ff", require('telescope.builtin').live_grep)
-		vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+		vim.keymap.set("n", "<space>ff", builtin.live_grep)
+		vim.keymap.set("n", "<space>fd", builtin.find_files)
 		-- VIM
-		vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-		vim.keymap.set("n", "<space>jp", require('telescope.builtin').jumplist)
+		vim.keymap.set("n", "<space>fh", builtin.help_tags)
+		vim.keymap.set("n", "<space>jp", builtin.jumplist)
 		vim.keymap.set("n", "<space>en", function()
-			require('telescope.builtin').find_files {
+			builtin.find_files {
 				cwd = vim.fn.stdpath("config")
 			}
 		end)
 		-- N7
 		vim.keymap.set("n", "<space>nda", function()
-			require('telescope.builtin').find_files {
+			builtin.find_files {
 				cwd = '/home/srshah/projects/nitro-api'
 			}
 		end)
 		vim.keymap.set("n", "<space>npw", function()
-			require('telescope.builtin').find_files {
+			builtin.find_files {
 				cwd = '/home/srshah/projects/sera-worker'
 			}
 		end)
