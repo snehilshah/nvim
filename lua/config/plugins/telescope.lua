81b3d28f6b8153934003debc45492599fec61d7f
snehilshah <snehilshah27@gmail.com>
2025-08-03 07:50:27 +0530

    telescope shortcuts

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,9 +7,7 @@ return {
 	},
 	config = function()
 		require('telescope').setup {
-			pickers = {
-				find_files = { theme = "ivy" }
-			},
+			defaults = require('telescope.themes').get_ivy(),
 			extensions = {
 				fzf = {}
 			}
@@ -17,14 +15,23 @@ return {
 
 		require('telescope').load_extension('fzf')
 
-		vim.keymap.set("n", "<space>fg", require('telescope.builtin').live_grep)
+		-- GIT
+		vim.keymap.set("n", "<space>gc", require('telescope.builtin').git_commits)
+		vim.keymap.set("n", "<space>gb", require('telescope.builtin').git_branches)
+		vim.keymap.set("n", "<space>gss", require('telescope.builtin').git_status)
+		vim.keymap.set("n", "<space>gst", require('telescope.builtin').git_stash)
+		-- FILES
+		vim.keymap.set("n", "<space>ff", require('telescope.builtin').live_grep)
 		vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+		-- VIM
 		vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+		vim.keymap.set("n", "<space>jp", require('telescope.builtin').jumplist)
 		vim.keymap.set("n", "<space>en", function()
 			require('telescope.builtin').find_files {
 				cwd = vim.fn.stdpath("config")
 			}
 		end)
+		-- N7
 		vim.keymap.set("n", "<space>nda", function()
 			require('telescope.builtin').find_files {
 				cwd = '/home/srshah/projects/nitro-api'
