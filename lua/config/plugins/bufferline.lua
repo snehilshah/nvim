52794d989825d0c2f4c69d0306df98521eec46cd
snehilshah <snehilshah27@gmail.com>
2025-08-18 10:17:13 +0530

    snacks and bufferline

---
+++ lua/config/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			mode = "buffers", -- set to "tabs" to only show tabpages instead
+			-- style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+			themable = true,
+			numbers = "ordinal",
+			close_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
+			right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+			left_mouse_command = "buffer %d", -- can be a string | function, | false see "Mouse actions"
+			middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
+			indicator = {
+				icon = "▎", -- this should be omitted if indicator style is not 'icon'
+				style = "icon",
+			},
+			diagnostics = "nvim_lsp", -- | "coc" | false, | "nvim_lsp" | "coc"
+			color_icons = true,
+			separator_style = "slant",
+		},
+	},
+}
