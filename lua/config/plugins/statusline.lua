04c2937384237044429348efd1a514b48e0743fd
snehilshah <snehilshah27@gmail.com>
2025-08-24 12:57:42 +0530

    comfy status line

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -2,15 +2,22 @@ return {
 
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "filename" },
-			test = {},
-			lualine_x = { "selectioncount" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
-	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
