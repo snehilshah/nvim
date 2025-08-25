bf013cbd23d10cefaa542779bba63bcdd4d51c80
snehilshah <snehilshah27@gmail.com>
2025-08-26 02:48:04 +0530

    new diagnostics

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -3,28 +3,28 @@ vim.lsp.enable({
     -- "ts-ls", removed in favour of typescript plugin
     "html-ls",
     "tailwindcss",
-    "lua-ls"
+    "lua-ls",
 })
 
-vim.diagnostic.config({
-    virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
-})
+-- vim.diagnostic.config({
+--     virtual_text = true,
+--     underline = true,
+--     update_in_insert = false,
+--     severity_sort = true,
+--     float = {
+--         border = "rounded",
+--         source = true,
+--     },
+--     signs = {
+--         text = {
+--             [vim.diagnostic.severity.ERROR] = "󰅚 ",
+--             [vim.diagnostic.severity.WARN] = "󰀪 ",
+--             [vim.diagnostic.severity.INFO] = "󰋽 ",
+--             [vim.diagnostic.severity.HINT] = "󰌶 ",
+--         },
+--         numhl = {
+--             [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+--             [vim.diagnostic.severity.WARN] = "WarningMsg",
+--         },
+--     },
+-- })
