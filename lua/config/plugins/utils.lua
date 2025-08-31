ba6fd105db3706ce37ec99ab3c0e0e5f82dad0f1
snehilshah <snehilshah27@gmail.com>
2025-09-01 03:25:49 +0530

    utiltity pluings

--- lua/config/plugins/utils.lua
+++ lua/config/plugins/utils.lua
@@ -236,4 +236,19 @@ return {
 			vim.keymap.set("v", "<leader>rs", require("lib.create").create_snippet)
 		end,
 	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				names = false, -- "Name" codes like Blue
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
 }
