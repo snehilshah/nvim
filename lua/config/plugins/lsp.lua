636e5c6270fd1ca65b0e838c3d4d2b9e199bec3d
snehilshah <snehilshah27@gmail.com>
2025-09-01 13:50:47 +0530

    better lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -20,64 +20,6 @@ return {
 			"folke/lazydev.nvim",
 		},
 		config = function()
-			local servers = {
-				gopls = {
-					cmd = { "gopls" },
-					filetypes = { "go", "gomod", "gowork", "gotmpl" },
-					root_dir = require("lspconfig").util.root_pattern("go.work", "go.mod", ".git"),
-					settings = {
-						gopls = {
-							staticcheck = true,
-							completeUnimported = true,
-							usePlaceholders = true,
-							completionDocumentation = true,
-							-- Code organization settings
-							gofumpt = true, -- Stricter formatting
-							codelenses = {
-								gc_details = true,
-								generate = true,
-								regenerate_cgo = true,
-								test = true,
-								tidy = true,
-								upgrade_dependency = true,
-								vendor = true,
-							},
-							diagnosticsDelay = "500ms",
-							buildFlags = { "-tags=integration,e2e" },
-							importShortcut = "Definition",
-							hints = {
-								assignVariableTypes = true,
-								compositeLiteralFields = true,
-								compositeLiteralTypes = true,
-								constantValues = true,
-								functionTypeParameters = true,
-								parameterNames = true,
-								rangeVariableTypes = true,
-							},
-							semanticTokens = true,
-							matcher = "Fuzzy",
-							symbolMatcher = "fuzzy",
-							symbolStyle = "Dynamic",
-							linkTarget = "pkg.go.dev",
-						},
-					},
-				},
-
-				lua_ls = {
-					filetypes = { "lua" },
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-							telemetry = {
-								enable = false,
-							},
-						},
-					},
-				},
-			}
-
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(args)
@@ -105,14 +47,18 @@ return {
 					-- map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					-- map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("gri", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					-- map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("grd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
 					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					-- Jump to the type definition of the word under your cursor.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
+					map("grt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
 					-- map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
@@ -124,6 +70,7 @@ return {
 					--  the definition of its *type*, not where it was *defined*.
 					-- map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 					-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gry", vim.lsp.buf.type_definition, opts)
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
