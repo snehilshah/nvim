3a2493ed4bb6dfee2d084cd77b7a71986a50b818
snehilshah <snehilshah27@gmail.com>
2025-09-19 11:18:24 +0530

    lint

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,45 +1,45 @@
 return {
-    'echasnovski/mini.nvim',
-    priority = 1000, -- Load early to ensure icons are available
-    lazy = false,    -- Load immediately, not lazily
-    config = function()
-        -- Setup mini.icons first (required for other mini modules and completions)
-        require('mini.icons').setup()
+	"echasnovski/mini.nvim",
+	priority = 1000, -- Load early to ensure icons are available
+	lazy = false, -- Load immediately, not lazily
+	config = function()
+		-- Setup mini.icons first (required for other mini modules and completions)
+		-- require('mini.icons').setup()
 
-        -- Better Around/Inside textobjects
-        --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-        --  - ci'  - [C]hange [I]nside [']quote
-        require('mini.ai').setup { n_lines = 500 }
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
 
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.surround").setup()
 
-        -- Note: mini.statusline is commented out since you have a custom statusline
-        -- If you want to use mini.statusline instead, uncomment the lines below
-        -- and remove the custom statusline require from init.lua
+		-- Note: mini.statusline is commented out since you have a custom statusline
+		-- If you want to use mini.statusline instead, uncomment the lines below
+		-- and remove the custom statusline require from init.lua
 
-        -- Simple and easy statusline.
-        --  You could remove this setup call if you don't like it,
-        --  and try some other statusline plugin
-        -- local statusline = require 'mini.statusline'
-        -- statusline.setup { use_icons = true }
+		-- Simple and easy statusline.
+		--  You could remove this setup call if you don't like it,
+		--  and try some other statusline plugin
+		-- local statusline = require 'mini.statusline'
+		-- statusline.setup { use_icons = true }
 
-        -- You can configure sections in the statusline by overriding their
-        -- default behavior. For example, here we set the section for
-        -- cursor location to LINE:COLUMN
-        ---@diagnostic disable-next-line: duplicate-set-field
-        -- statusline.section_location = function()
-        --     return '%2l:%-2v'
-        -- end
+		-- You can configure sections in the statusline by overriding their
+		-- default behavior. For example, here we set the section for
+		-- cursor location to LINE:COLUMN
+		---@diagnostic disable-next-line: duplicate-set-field
+		-- statusline.section_location = function()
+		--     return '%2l:%-2v'
+		-- end
 
-        -- ... and there is more!
-        --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
+		-- ... and there is more!
+		--  Check out: https://github.com/echasnovski/mini.nvim
+	end,
 }
