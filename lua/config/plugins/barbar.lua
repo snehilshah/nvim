17276c69402ac3fd34d6882c8a2d074dd9cbaefe
snehilshah <snehilshah27@gmail.com>
2025-09-22 18:54:51 +0530

    diagnostics in barbar and git status in lualine

--- lua/config/plugins/barbar.lua
+++ lua/config/plugins/barbar.lua
@@ -1,72 +1,55 @@
 return {
 	"romgrk/barbar.nvim",
 	dependencies = {
-		"lewis6991/gitsigns.nvim",   -- OPTIONAL: for git status
 		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 	},
 	init = function()
 		vim.g.barbar_auto_setup = false
 	end,
-	config = function()
-		vim.g.barbar_auto_setup = false -- disable auto-setup
-		require("barbar").setup({
-			animation = true,
-
-			-- Enable/disable current/total tabpages indicator (top right corner)
-			tabpages = true,
-
-			-- A buffer to this direction will be focused (if it exists) when closing the current buffer.
-			-- Valid options are 'left' (the default), 'previous', and 'right'
-			focus_on_close = "left",
-
-			-- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-			hide = { extensions = false, inactive = false },
-
-			icons = {
-				buffer_index = false,
-				buffer_number = false,
-				button = "",
-				diagnostics = {
-					[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
-				},
-				gitsigns = {
-					added = { enabled = true, icon = " " },
-					changed = { enabled = true, icon = " " },
-					deleted = { enabled = true, icon = " " },
-				},
-				separator = { left = "▎", right = "" },
-
-				-- If true, add an additional separator at the end of the buffer list
-				separator_at_end = true,
-
-				-- Configure the icons on the bufferline when modified or pinned.
-				-- Supports all the base icon options.
-				modified = { button = "●" },
-				pinned = { button = "", filename = true },
-
-				-- Configure the icons on the bufferline based on the visibility of a buffer.
-				-- Supports all the base icon options, plus `modified` and `pinned`.
-				alternate = { filetype = { enabled = false } },
-				current = { buffer_index = true },
-				inactive = { button = "×" },
-				visible = { modified = { buffer_number = false } },
+	opts = {
+		animation = true,
+		-- Enable/disable current/total tabpages indicator (top right corner)
+		tabpages = true,
+		-- A buffer to this direction will be focused (if it exists) when closing the current buffer.
+		-- Valid options are 'left' (the default), 'previous', and 'right'
+		focus_on_close = "left",
+		-- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+		hide = { extensions = false, inactive = false },
+		icons = {
+			buffer_index = false,
+			buffer_number = false,
+			button = "",
+			diagnostics = {
+				[vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
 			},
-
-			sidebar_filetypes = { -- Set the filetypes which barbar will offset itself for
-				-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-				NvimTree = true,
-				-- Or, specify the text used for the offset:
-				undotree = {
-					text = "undotree",
-					align = "left", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-				},
-				-- Or, specify the event which the sidebar executes when leaving:
-				["neo-tree"] = { event = "BufWipeout" },
-				-- Or, specify all three
-				Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
+			separator = { left = "▎", right = "" },
+			-- If true, add an additional separator at the end of the buffer list
+			separator_at_end = true,
+			-- Configure the icons on the bufferline when modified or pinned.
+			-- Supports all the base icon options.
+			modified = { button = "●" },
+			pinned = { button = "", filename = true },
+			-- Configure the icons on the bufferline based on the visibility of a buffer.
+			-- Supports all the base icon options, plus `modified` and `pinned`.
+			alternate = { filetype = { enabled = false } },
+			current = { buffer_index = true },
+			inactive = { button = "×" },
+			visible = { modified = { buffer_number = false } },
+		},
+		sidebar_filetypes = { -- Set the filetypes which barbar will offset itself for
+			-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+			NvimTree = true,
+			-- Or, specify the text used for the offset:
+			undotree = {
+				text = "undotree",
+				align = "left", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
 			},
-			maximum_length = 25, -- Sets the maximum buffer name length.
-		})
-	end,
+			-- Or, specify the event which the sidebar executes when leaving:
+			["neo-tree"] = { event = "BufWipeout" },
+			-- Or, specify all three
+			Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
+		},
+		maximum_length = 25, -- Sets the maximum buffer name length.
+	},
 	version = "^1.0.0", -- optional: only update when a new 1.x version is released
 }
