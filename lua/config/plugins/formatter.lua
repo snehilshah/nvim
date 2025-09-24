2d688d61b0b1e07b0a0c460bd5f4e2625703f075
snehilshah <snehilshah27@gmail.com>
2025-09-24 17:57:34 +0530

    keymab for [F]ormat [B]uffer

--- lua/config/plugins/formatter.lua
+++ lua/config/plugins/formatter.lua
@@ -4,7 +4,7 @@ return {
 	cmd = { "ConformInfo" },
 	keys = {
 		{
-			"<leader>af",
+			"<leader>fb",
 			function()
 				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
