18b0fe2c1f2ec8e62a86009329b98cb74e02b2bf
snehilshah <snehilshah27@gmail.com>
2025-11-03 04:31:20 +0530

    remove nvim surround in favour of mini.surround

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -31,14 +31,6 @@ return {
 	},
 	{ "tpope/vim-sleuth" },
 	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	{
 		"bkad/CamelCaseMotion",
 		init = function()
 			vim.g.camelcasemotion_key = "<leader>"
