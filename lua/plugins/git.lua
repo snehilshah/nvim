a6a3a57a0ef4a176af4538be1cb8ff0f5005e316
snehilshah <snehilshah27@gmail.com>
2025-11-03 03:45:25 +0530

    neogit instaed of fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -164,673 +164,40 @@ return {
 		"sindrets/diffview.nvim",
 		event = "VeryLazy",
 		cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
-		opts = {
-			diff_binaries = false,
-			enhanced_diff_hl = false,
-			git = {
-				max_files = 300,
-			},
-			file_panel = {
-				listing_style = "tree",
-				tree_options = {
-					flatten_dirs = true,
-					folder_statuses = "only_folded",
-				},
-				win_config = {
-					position = "left",
-					width = 35,
-				},
-			},
-			file_history_panel = {
-				log_options = {
-					git = {
-						single_file = {
-							diff_merges = "combined",
-						},
-						multi_file = {
-							diff_merges = "first-parent",
-						},
-					},
-				},
-				win_config = {
-					position = "bottom",
-					height = 16,
-				},
-			},
-			commit_log_panel = {
-				win_config = {
-					position = "bottom",
-					height = 16,
-				},
-			},
-			default_args = {
-				DiffviewOpen = {},
-				DiffviewFileHistory = {},
-			},
-			hooks = {},
-			keymaps = {
-				disable_defaults = false,
-				view = {
-					{
-						"n",
-						"<tab>",
-						function()
-							require("diffview.actions").select_next_entry()
-						end,
-						{ desc = "Next file" },
-					},
-					{
-						"n",
-						"<s-tab>",
-						function()
-							require("diffview.actions").select_prev_entry()
-						end,
-						{ desc = "Previous file" },
-					},
-					{
-						"n",
-						"gf",
-						function()
-							require("diffview.actions").goto_file()
-						end,
-						{ desc = "Go to file" },
-					},
-					{
-						"n",
-						"<C-w><C-f>",
-						function()
-							require("diffview.actions").goto_file_split()
-						end,
-						{ desc = "Go to file in split" },
-					},
-					{
-						"n",
-						"<C-w>gf",
-						function()
-							require("diffview.actions").goto_file_tab()
-						end,
-						{ desc = "Go to file in tab" },
-					},
-					{
-						"n",
-						"<leader>b",
-						function()
-							require("diffview.actions").toggle_files()
-						end,
-						{ desc = "Toggle files panel" },
-					},
-					{
-						"n",
-						"g<C-x>",
-						function()
-							require("diffview.actions").cycle_layout()
-						end,
-						{ desc = "Cycle layout" },
-					},
-					{
-						"n",
-						"[x",
-						function()
-							require("diffview.actions").prev_conflict()
-						end,
-						{ desc = "Previous conflict" },
-					},
-					{
-						"n",
-						"]x",
-						function()
-							require("diffview.actions").next_conflict()
-						end,
-						{ desc = "Next conflict" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("view")
-						end,
-						{ desc = "Show help" },
-					},
-				},
-				diff1 = {
-					-- Mappings in single commit diff
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("view")
-						end,
-						{ desc = "Show help" },
-					},
-				},
-				diff2 = {
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("view")
-						end,
-						{ desc = "Show help" },
-					},
-				},
-				diff3 = {
-					-- Mappings in 3-way diffs (during merge conflicts)
-					{
-						{ "n",                   "x" },
-						"2do",
-						function()
-							require("diffview.actions").diffget("ours")
-						end,
-						{ desc = "Get from OURS" },
-					},
-					{
-						{ "n",                     "x" },
-						"3do",
-						function()
-							require("diffview.actions").diffget("theirs")
-						end,
-						{ desc = "Get from THEIRS" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("diff3")
-						end,
-						{ desc = "Show help" },
-					},
-				},
-				diff4 = {
-					{
-						{ "n",                   "x" },
-						"1do",
-						function()
-							require("diffview.actions").diffget("base")
-						end,
-						{ desc = "Get from BASE" },
-					},
-					{
-						{ "n",                   "x" },
-						"2do",
-						function()
-							require("diffview.actions").diffget("ours")
-						end,
-						{ desc = "Get from OURS" },
-					},
-					{
-						{ "n",                     "x" },
-						"3do",
-						function()
-							require("diffview.actions").diffget("theirs")
-						end,
-						{ desc = "Get from THEIRS" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("diff4")
-						end,
-						{ desc = "Show help" },
-					},
-				},
+		config = function()
+			local actions = require("diffview.actions")
+			require("diffview").setup({
+				enhanced_diff_hl = true,
 				file_panel = {
-					-- File panel specific mappings
-					{
-						"n",
-						"j",
-						function()
-							require("diffview.actions").next_entry()
-						end,
-						{ desc = "Next entry" },
-					},
-					{
-						"n",
-						"<down>",
-						function()
-							require("diffview.actions").next_entry()
-						end,
-						{ desc = "Next entry" },
-					},
-					{
-						"n",
-						"k",
-						function()
-							require("diffview.actions").prev_entry()
-						end,
-						{ desc = "Previous entry" },
-					},
-					{
-						"n",
-						"<up>",
-						function()
-							require("diffview.actions").prev_entry()
-						end,
-						{ desc = "Previous entry" },
-					},
-					{
-						"n",
-						"<cr>",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"o",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"<2-LeftMouse>",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"-",
-						function()
-							require("diffview.actions").toggle_stage_entry()
-						end,
-						{ desc = "Stage/unstage entry" },
-					},
-					{
-						"n",
-						"S",
-						function()
-							require("diffview.actions").stage_all()
-						end,
-						{ desc = "Stage all" },
-					},
-					{
-						"n",
-						"U",
-						function()
-							require("diffview.actions").unstage_all()
-						end,
-						{ desc = "Unstage all" },
-					},
-					{
-						"n",
-						"X",
-						function()
-							require("diffview.actions").restore_entry()
-						end,
-						{ desc = "Restore entry" },
-					},
-					{
-						"n",
-						"L",
-						function()
-							require("diffview.actions").open_commit_log()
-						end,
-						{ desc = "Open commit log" },
-					},
-					{
-						"n",
-						"zo",
-						function()
-							require("diffview.actions").open_fold()
-						end,
-						{ desc = "Expand fold" },
-					},
-					{
-						"n",
-						"zc",
-						function()
-							require("diffview.actions").close_fold()
-						end,
-						{ desc = "Collapse fold" },
-					},
-					{
-						"n",
-						"za",
-						function()
-							require("diffview.actions").toggle_fold()
-						end,
-						{ desc = "Toggle fold" },
-					},
-					{
-						"n",
-						"zR",
-						function()
-							require("diffview.actions").open_all_folds()
-						end,
-						{ desc = "Expand all folds" },
-					},
-					{
-						"n",
-						"zM",
-						function()
-							require("diffview.actions").close_all_folds()
-						end,
-						{ desc = "Collapse all folds" },
-					},
-					{
-						"n",
-						"<c-b>",
-						function()
-							require("diffview.actions").scroll_view(-0.25)
-						end,
-						{ desc = "Scroll up" },
-					},
-					{
-						"n",
-						"<c-f>",
-						function()
-							require("diffview.actions").scroll_view(0.25)
-						end,
-						{ desc = "Scroll down" },
-					},
-					{
-						"n",
-						"<tab>",
-						function()
-							require("diffview.actions").select_next_entry()
-						end,
-						{ desc = "Next file" },
-					},
-					{
-						"n",
-						"<s-tab>",
-						function()
-							require("diffview.actions").select_prev_entry()
-						end,
-						{ desc = "Previous file" },
-					},
-					{
-						"n",
-						"gf",
-						function()
-							require("diffview.actions").goto_file()
-						end,
-						{ desc = "Go to file" },
-					},
-					{
-						"n",
-						"<C-w><C-f>",
-						function()
-							require("diffview.actions").goto_file_split()
-						end,
-						{ desc = "Go to file in split" },
-					},
-					{
-						"n",
-						"<C-w>gf",
-						function()
-							require("diffview.actions").goto_file_tab()
-						end,
-						{ desc = "Go to file in tab" },
-					},
-					{
-						"n",
-						"i",
-						function()
-							require("diffview.actions").listing_style()
-						end,
-						{ desc = "Toggle listing style" },
-					},
-					{
-						"n",
-						"f",
-						function()
-							require("diffview.actions").toggle_flatten_dirs()
-						end,
-						{ desc = "Toggle flatten dirs" },
-					},
-					{
-						"n",
-						"R",
-						function()
-							require("diffview.actions").refresh_files()
-						end,
-						{ desc = "Refresh files" },
-					},
-					{
-						"n",
-						"<leader>b",
-						function()
-							require("diffview.actions").toggle_files()
-						end,
-						{ desc = "Toggle files panel" },
-					},
-					{
-						"n",
-						"g<C-x>",
-						function()
-							require("diffview.actions").cycle_layout()
-						end,
-						{ desc = "Cycle layout" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("file_panel")
-						end,
-						{ desc = "Show help" },
+					listing_style = "tree",
+					tree_options = {
+						flatten_dirs = false,
 					},
 				},
 				file_history_panel = {
-					{
-						"n",
-						"g!",
-						function()
-							require("diffview.actions").options()
-						end,
-						{ desc = "Open options" },
+					log_options = {
+						git = {
+							single_file = {
+								diff_merges = "combined",
+							},
+							multi_file = {
+								diff_merges = "first-parent",
+							},
+						},
 					},
-					{
-						"n",
-						"<C-A-d>",
-						function()
-							require("diffview.actions").open_in_diffview()
-						end,
-						{ desc = "Open in diffview" },
-					},
-					{
-						"n",
-						"y",
-						function()
-							require("diffview.actions").copy_hash()
-						end,
-						{ desc = "Copy commit hash" },
-					},
-					{
-						"n",
-						"L",
-						function()
-							require("diffview.actions").open_commit_log()
-						end,
-						{ desc = "Open commit log" },
-					},
-					{
-						"n",
-						"zR",
-						function()
-							require("diffview.actions").open_all_folds()
-						end,
-						{ desc = "Expand all folds" },
-					},
-					{
-						"n",
-						"zM",
-						function()
-							require("diffview.actions").close_all_folds()
-						end,
-						{ desc = "Collapse all folds" },
-					},
-					{
-						"n",
-						"j",
-						function()
-							require("diffview.actions").next_entry()
-						end,
-						{ desc = "Next entry" },
-					},
-					{
-						"n",
-						"<down>",
-						function()
-							require("diffview.actions").next_entry()
-						end,
-						{ desc = "Next entry" },
-					},
-					{
-						"n",
-						"k",
-						function()
-							require("diffview.actions").prev_entry()
-						end,
-						{ desc = "Previous entry" },
-					},
-					{
-						"n",
-						"<up>",
-						function()
-							require("diffview.actions").prev_entry()
-						end,
-						{ desc = "Previous entry" },
-					},
-					{
-						"n",
-						"<cr>",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"o",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"<2-LeftMouse>",
-						function()
-							require("diffview.actions").select_entry()
-						end,
-						{ desc = "Select entry" },
-					},
-					{
-						"n",
-						"<c-b>",
-						function()
-							require("diffview.actions").scroll_view(-0.25)
-						end,
-						{ desc = "Scroll up" },
-					},
-					{
-						"n",
-						"<c-f>",
-						function()
-							require("diffview.actions").scroll_view(0.25)
-						end,
-						{ desc = "Scroll down" },
-					},
-					{
-						"n",
-						"<tab>",
-						function()
-							require("diffview.actions").select_next_entry()
-						end,
-						{ desc = "Next file" },
-					},
-					{
-						"n",
-						"<s-tab>",
-						function()
-							require("diffview.actions").select_prev_entry()
-						end,
-						{ desc = "Previous file" },
-					},
-					{
-						"n",
-						"gf",
-						function()
-							require("diffview.actions").goto_file()
-						end,
-						{ desc = "Go to file" },
-					},
-					{
-						"n",
-						"<C-w><C-f>",
-						function()
-							require("diffview.actions").goto_file_split()
-						end,
-						{ desc = "Go to file in split" },
-					},
-					{
-						"n",
-						"<C-w>gf",
-						function()
-							require("diffview.actions").goto_file_tab()
-						end,
-						{ desc = "Go to file in tab" },
-					},
-					{
-						"n",
-						"<leader>b",
-						function()
-							require("diffview.actions").toggle_files()
-						end,
-						{ desc = "Toggle files panel" },
-					},
-					{
-						"n",
-						"g<C-x>",
-						function()
-							require("diffview.actions").cycle_layout()
-						end,
-						{ desc = "Cycle layout" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("file_history_panel")
-						end,
-						{ desc = "Show help" },
+					win_config = {
+						position = "bottom",
+						height = 16,
 					},
 				},
-				option_panel = {
-					{
-						"n",
-						"<tab>",
-						function()
-							require("diffview.actions").select()
-						end,
-						{ desc = "Select" },
-					},
-					{
-						"n",
-						"q",
-						function()
-							require("diffview.actions").close()
-						end,
-						{ desc = "Close" },
-					},
-					{
-						"n",
-						"g?",
-						function()
-							require("diffview.actions").help("option_panel")
-						end,
-						{ desc = "Show help" },
+				keymaps = {
+					file_panel = {
+						{ "n", "<tab>", actions.toggle_stage_entry, { desc = "Open the diff for the current file" } },
+						{ "n", "<cr>",  actions.goto_file_edit,     { desc = "go to edit the file" } },
 					},
 				},
-			},
-		},
+			})
+		end,
 		keys = {
 			{
 				"<leader>Dv",
@@ -872,14 +239,34 @@ return {
 				end,
 				desc = "Compare with specific branch",
 			},
+		},
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Neogit",
+		keys = {
 			{
-				"<leader>Gc",
-				":DiffviewOpen --imply-local<CR>",
-				desc = "Open merge conflicts",
+				"<leader>gg",
+				"<cmd>Neogit<cr>",
+				desc = "Neogit",
+			},
+			{
+				"<leader>gc",
+				"<cmd>Neogit commit<cr>",
+				desc = "[c]ommit",
+			},
+		},
+		opts = {
+			integrations = {
+				diffview = true,
 			},
 		},
 	},
-	"tpope/vim-fugitive",
 	{
 		"mbbill/undotree",
 		keys = {
