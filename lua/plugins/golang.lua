d30b6d9e6e9925fb78317d359af552d2fd82bdf9
snehilshah <snehilshah27@gmail.com>
2025-11-11 13:47:40 +0530

    improve golang lsp

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -1,16 +1,84 @@
 return {
 	"ray-x/go.nvim",
-	enabled = false,
-	dependencies = { -- optional packages
+	enabled = true,
+	dependencies = {
 		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
 		"nvim-treesitter/nvim-treesitter",
+		"neovim/nvim-lspconfig",
 	},
 	opts = {
 		lsp_keymaps = false,
+		lsp_on_attach = false,
+
+		lsp_cfg = {
+			settings = {
+				gopls = {
+					gofumpt = true,
+					codelenses = {
+						gc_details = false,
+						generate = true,
+						regenerate_cgo = true,
+						run_govulncheck = true,
+						test = true,
+						tidy = true,
+						upgrade_dependency = true,
+						vendor = true,
+					},
+					hints = {
+						assignVariableTypes = false,
+						compositeLiteralFields = false,
+						compositeLiteralTypes = false,
+						constantValues = true,
+						functionTypeParameters = false,
+						parameterNames = true,
+						rangeVariableTypes = true,
+					},
+					analyses = {
+						nilness = true,
+						unusedparams = true,
+						unusedwrite = true,
+						useany = true,
+						unreachable = true,
+						unusedresult = true,
+						simplifyslice = true,
+						simplifyrange = true,
+						simplifycompositelit = true,
+						shadow = false,
+						printf = false,
+						structtag = true,
+						modernize = true,
+						stylecheck = true,
+						gocritic = true,
+						deprecated = true,
+					},
+					usePlaceholders = true,
+					completeUnimported = true,
+					staticcheck = true,
+					directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+					semanticTokens = true,
+				},
+			},
+		},
+
+		lsp_gofumpt = true,
+
+		-- Disable go.nvim's diagnostic display to avoid conflicts with tiny-diagnostics
+		lsp_diag_virtual_text = false,
+		lsp_diag_signs = false,
+		lsp_diag_update_in_insert = false,
+
+		-- Alternatively, use this diagnostic config to completely disable go.nvim diagnostics
+		diagnostic = false,
+
+		-- Enable inlay hints
+		lsp_inlay_hints = {
+			enable = true,
+		},
 	},
-	config = function(lp, opts)
+	config = function(_, opts)
 		require("go").setup(opts)
+
+		-- Auto-format with goimports on save
 		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*.go",
@@ -22,5 +90,5 @@ return {
 	end,
 	event = { "CmdlineEnter" },
 	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	build = ':lua require("go.install").update_all_sync()',
 }
