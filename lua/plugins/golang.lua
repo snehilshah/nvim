6407761ea21c33cbfd881fa0acab158c436e7679
snehilshah <snehilshah27@gmail.com>
2025-11-18 01:56:16 +0530

    fix golang lsp and linting

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -7,78 +7,22 @@ return {
 		"neovim/nvim-lspconfig",
 	},
 	opts = {
+		lsp_cfg = true,
 		lsp_keymaps = false,
-		lsp_on_attach = false,
-
-		lsp_cfg = {
-			settings = {
-				gopls = {
-					gofumpt = true,
-					codelenses = {
-						gc_details = false,
-						generate = true,
-						regenerate_cgo = true,
-						run_govulncheck = true,
-						test = true,
-						tidy = true,
-						upgrade_dependency = true,
-						vendor = true,
-					},
-					hints = {
-						assignVariableTypes = false,
-						compositeLiteralFields = false,
-						compositeLiteralTypes = false,
-						constantValues = true,
-						functionTypeParameters = false,
-						parameterNames = true,
-						rangeVariableTypes = true,
-					},
-					analyses = {
-						nilness = true,
-						unusedparams = true,
-						unusedwrite = true,
-						useany = true,
-						unreachable = true,
-						unusedresult = true,
-						simplifyslice = true,
-						simplifyrange = true,
-						simplifycompositelit = true,
-						shadow = false,
-						printf = false,
-						structtag = true,
-						modernize = true,
-						stylecheck = true,
-						gocritic = true,
-						deprecated = true,
-					},
-					usePlaceholders = true,
-					completeUnimported = true,
-					staticcheck = true,
-					directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-					semanticTokens = true,
-				},
-			},
-		},
-
+		lsp_on_attach = true,
 		lsp_gofumpt = true,
-
-		-- Disable go.nvim's diagnostic display to avoid conflicts with tiny-diagnostics
-		lsp_diag_virtual_text = false,
-		lsp_diag_signs = false,
-		lsp_diag_update_in_insert = false,
-
-		-- Alternatively, use this diagnostic config to completely disable go.nvim diagnostics
-		diagnostic = false,
-
-		-- Enable inlay hints
 		lsp_inlay_hints = {
 			enable = true,
 		},
+
+		-- these are disabled to avoid conflicts with tiny-diagnostics
+		diagnostic = false,
+		lsp_diag_virtual_text = true,
+		lsp_diag_signs = true,
+		lsp_diag_update_in_insert = true,
 	},
 	config = function(_, opts)
 		require("go").setup(opts)
-
-		-- Auto-format with goimports on save
 		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*.go",
@@ -88,7 +32,7 @@ return {
 			group = format_sync_grp,
 		})
 	end,
-	event = { "CmdlineEnter" },
+	event = { "BufReadPost" },
 	ft = { "go", "gomod" },
 	build = ':lua require("go.install").update_all_sync()',
 }
