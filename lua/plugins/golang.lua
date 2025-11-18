d17feb3294c781f84a4c702522cf935c5e7e45b4
snehilshah <snehilshah27@gmail.com>
2025-11-18 18:07:51 +0530

    configure no-go.nvim from custom config

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -1,37 +1,48 @@
 return {
-	"ray-x/go.nvim",
-	enabled = true,
-	dependencies = {
-		"ray-x/guihua.lua",
-		"nvim-treesitter/nvim-treesitter",
-		"neovim/nvim-lspconfig",
-	},
-	opts = {
-		lsp_cfg = true,
-		lsp_keymaps = false,
-		lsp_on_attach = true,
-		lsp_gofumpt = true,
-		lsp_inlay_hints = {
-			enable = true,
+	{
+		"ray-x/go.nvim",
+		enabled = true,
+		dependencies = {
+			"ray-x/guihua.lua",
+			"nvim-treesitter/nvim-treesitter",
+			"neovim/nvim-lspconfig",
 		},
+		opts = {
+			lsp_cfg = true,
+			lsp_keymaps = false,
+			lsp_on_attach = true,
+			lsp_gofumpt = true,
+			lsp_inlay_hints = {
+				enable = true,
+			},
 
-		-- these are disabled to avoid conflicts with tiny-diagnostics
-		diagnostic = false,
-		lsp_diag_virtual_text = true,
-		lsp_diag_signs = true,
-		lsp_diag_update_in_insert = true,
+			-- these are disabled to avoid conflicts with tiny-diagnostics
+			diagnostic = false,
+			lsp_diag_virtual_text = true,
+			lsp_diag_signs = true,
+			lsp_diag_update_in_insert = true,
+		},
+		config = function(_, opts)
+			require("go").setup(opts)
+			local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					require("go.format").goimports()
+				end,
+				group = format_sync_grp,
+			})
+		end,
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
 	},
-	config = function(_, opts)
-		require("go").setup(opts)
-		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*.go",
-			callback = function()
-				require("go.format").goimports()
-			end,
-			group = format_sync_grp,
-		})
-	end,
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()',
+	{
+		"snehilshah/no-go.nvim",
+		enabled = true,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		ft = "go",
+		opts = {
+			identifiers = { "err", "error" },
+		},
+	},
 }
