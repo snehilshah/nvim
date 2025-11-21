508f1f8b1175eeb9ddc9dfe6ec91380603b53025
snehilshah <snehilshah27@gmail.com>
2025-11-21 12:56:34 +0530

    configure go lsp with semantic higlights

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -6,24 +6,93 @@ return {
 			"ray-x/guihua.lua",
 			"nvim-treesitter/nvim-treesitter",
 			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			lsp_cfg = true,
-			lsp_keymaps = false,
-			lsp_on_attach = true,
-			lsp_gofumpt = true,
-			lsp_inlay_hints = {
-				enable = true,
-			},
-
-			-- these are disabled to avoid conflicts with tiny-diagnostics
-			diagnostic = false,
-			lsp_diag_virtual_text = true,
-			lsp_diag_signs = true,
-			lsp_diag_update_in_insert = true,
+			"saghen/blink.cmp",
 		},
 		config = function(_, opts)
-			require("go").setup(opts)
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			require("go").setup({
+				lsp_keymaps = true,
+				lsp_codelens = true,
+				lsp_on_attach = true,
+				lsp_gofumpt = true,
+				lsp_inlay_hints = {
+					enable = true,
+				},
+				lsp_semantic_highlights = true,
+				lsp_cfg = {
+					capabilities = capabilities,
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = true,
+								generate = true,
+								regenerate_cgo = false,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+								unreachable = true,
+								unusedresult = true,
+								simplifyslice = true,
+								simplifyrange = true,
+								simplifycompositelit = true,
+								shadow = false,
+								printf = true,
+								structtag = true,
+								modernize = true,
+								stylecheck = true,
+								gocritic = true,
+								deprecated = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = {
+								"-.git",
+								"-.vscode",
+								"-.idea",
+								"-.vscode-test",
+								"-node_modules",
+								"-generated",
+							},
+							semanticTokens = true,
+						},
+					},
+				},
+				golangci_lint = {
+					default = 'all', -- set to one of { 'standard', 'fast', 'all', 'none' }
+					-- disable = {'errcheck', 'staticcheck'}, -- linters to disable empty by default
+					-- enable = {'govet', 'ineffassign','revive', 'gosimple'}, -- linters to enable; empty by default
+					config = nil, -- set to a config file path
+					no_config = false, -- true: golangci-lint --no-config
+					-- disable = {},     -- linters to disable empty by default, e.g. {'errcheck', 'staticcheck'}
+					-- enable = {},      -- linters to enable; empty by default, set to e.g. {'govet', 'ineffassign','revive', 'gosimple'}
+					-- enable_only = {}, -- linters to enable only; empty by default, set to e.g. {'govet', 'ineffassign','revive', 'gosimple'}
+					severity = vim.diagnostic.severity.INFO, -- severity level of the diagnostics
+				},
+				-- these are disabled to avoid conflicts with tiny-diagnostics
+				diagnostic = false,
+				lsp_diag_virtual_text = true,
+				lsp_diag_signs = true,
+				lsp_diag_update_in_insert = true,
+			})
 			local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				pattern = "*.go",
