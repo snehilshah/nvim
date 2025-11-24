e9b8cfd2dcde7508cf2d311d5233894fc6ae835c
snehilshah <snehilshah27@gmail.com>
2025-11-24 06:08:18 +0530

    configured own lsp configuration for good

---
+++ after/lsp/ts_ls.lua
@@ -0,0 +1,48 @@
+return {
+    settings = {
+        typescript = {
+            format = {
+                trimTrailingWhitespace = true,
+            },
+            implementationsCodeLens = {
+                enabled = true
+            },
+            referencesCodeLens = {
+                enabled = true,
+                showOnAllFunctions = true
+            },
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true
+            },
+        },
+        javascript = {
+            format = {
+                trimTrailingWhitespace = true,
+            },
+            implementationsCodeLens = {
+                enabled = true
+            },
+            referencesCodeLens = {
+                enabled = true,
+                showOnAllFunctions = true
+            },
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true
+            },
+        },
+    },
+}
