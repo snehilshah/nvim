3a301f7135fcb2523055b2e88c048f3e43be4678
snehilshah <snehilshah27@gmail.com>
2025-11-28 12:17:45 +0530

    customize icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,7 +6,29 @@ return {
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 }) -- this is not a "AI" plugin
 		require("mini.cursorword").setup()
-		require("mini.icons").setup()
+		require("mini.icons").setup({
+			-- Override by exact filename (case-sensitive)
+			file = {
+				["Dockerfile"] = { glyph = "󰡨", hl = "MiniIconsBlue" },
+				["Makefile"] = { glyph = "", hl = "MiniIconsRed" },
+				[".dockerignore"] = { glyph = "󰡨", hl = "MiniIconsBlue" },
+				[".gitignore"] = { glyph = "", hl = "MiniIconsOrange" },
+				["README.md"] = { glyph = "", hl = "MiniIconsRed" },
+			},
+			-- Override by filetype
+			filetype = {
+				dockerfile = { glyph = "󰡨", hl = "MiniIconsBlue" },
+				make = { glyph = "", hl = "MiniIconsRed" },
+				markdown = { glyph = "", hl = "MiniIconsBlue" },
+				go = { glyph = "", hl = "MiniIconsAzure" },
+				json = { glyph = "", hl = "MiniIconsYellow" },
+				jsonc = { glyph = "", hl = "MiniIconsYellow" },
+			},
+			extension = {
+				log = { glyph = "󰌱", hl = "MiniIconsGreen" },
+				md = { glyph = "", hl = "MiniIconsBlue" },
+			},
+		})
 		require("mini.operators").setup()
 		require("mini.surround").setup()
 	end,
