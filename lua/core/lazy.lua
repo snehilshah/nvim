f5a9004cbe6aaa7777c2f1739604bcdb9c8a8fd9
snehilshah <snehilshah27@gmail.com>
2025-12-01 05:12:40 +0530

    configure theme via lazy plugin manager

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
+			{ out, "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -20,8 +20,8 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
-	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "gruvbox-material" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
