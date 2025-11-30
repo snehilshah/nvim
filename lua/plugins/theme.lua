e42da7489767f2db7073d997ddbc664d084828cc
snehilshah <snehilshah27@gmail.com>
2025-12-01 05:14:02 +0530

    some theme changes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,26 +9,26 @@ return {
 		vim.g.gruvbox_material_background = "hard"
 		vim.g.gruvbox_material_ui_contrast = "high"
 		vim.g.gruvbox_material_float_style = "bright"
-		vim.g.gruvbox_material_statusline_style = "mix" -- Options: "original", "material", "mix", "afterglow"
+		vim.g.gruvbox_material_statusline_style = "material" -- Options: "original", "material", "mix", "afterglow"
 		vim.g.gruvbox_material_cursor = "auto"
+		vim.g.gruvbox_material_current_word = "underline" -- Options: "grey background", "bold", "underline", "italic"
 
 		-- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
 		-- vim.g.gruvbox_material_better_performance = 1
 
-		-- vim.cmd.colorscheme("gruvbox-material")
+		vim.cmd.colorscheme("gruvbox-material")
 
 		-- Custom statusline highlights
-		-- vim.api.nvim_set_hl(0, "StatusLine", {
-		--   bg = "#1C2021", -- Dark gray background
-		--   fg = "#ebdbb2", -- Light text
-		--   bold = false
-		-- })
-		--
-		-- vim.api.nvim_set_hl(0, "StatusLineNC", {
-		--   bg = "#1C2021", -- Darker background for inactive windows
-		--   fg = "#928374", -- Muted text
-		--   bold = false
-		-- })
-		vim.cmd("colorscheme gruvbox-material")
+		vim.api.nvim_set_hl(0, "StatusLine", {
+			bg = "#1C2021", -- Dark gray background
+			fg = "#ebdbb2", -- Light text
+			bold = false,
+		})
+
+		vim.api.nvim_set_hl(0, "StatusLineNC", {
+			bg = "#1C2021", -- Darker background for inactive windows
+			fg = "#928374", -- Muted text
+			bold = false,
+		})
 	end,
 }
