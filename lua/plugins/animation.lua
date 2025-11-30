b8997e4f9819dfd68bb31fdba757b7fd5a1e1951
snehilshah <snehilshah27@gmail.com>
2025-12-01 05:29:35 +0530

    no need of yanky for paste

--- lua/plugins/animation.lua
+++ lua/plugins/animation.lua
@@ -1,153 +1,147 @@
 return {
-
-	{
-		"y3owk1n/undo-glow.nvim",
-		event = { "VeryLazy" },
-		enabled = true,
-		opts = {
-			animation = {
-				enabled = true,
-				duration = 600,
-				animation_type = "zoom",
-				window_scoped = true,
-			},
-			highlights = {
-				undo = {
-					hl_color = { bg = "#693232" }, -- Dark muted red
-				},
-				redo = {
-					hl_color = { bg = "#2F4640" }, -- Dark muted green
-				},
-				yank = {
-					hl_color = { bg = "#7A683A" }, -- Dark muted yellow
-				},
-				paste = {
-					hl_color = { bg = "#325B5B" }, -- Dark muted cyan
-				},
-				-- search = {
-				-- 	hl_color = { bg = "#5C475C" }, -- Dark muted purple
-				-- },
-				comment = {
-					hl_color = { bg = "#7A5A3D" }, -- Dark muted orange
-				},
-				cursor = {
-					hl_color = { bg = "#793D54" }, -- Dark muted pink
-				},
-			},
-			priority = 2048 * 3,
+	"y3owk1n/undo-glow.nvim",
+	enabled = true,
+	opts = {
+		animation = {
+			enabled = true,
+			duration = 600,
+			animation_type = "zoom",
+			window_scoped = true,
 		},
-		keys = {
-			{
-				"u",
-				function()
-					require("undo-glow").undo({
-						animation = {
-							animation_type = "fade",
-						},
-					})
-				end,
-				mode = "n",
-				desc = "Undo with highlight",
-				noremap = true,
+		highlights = {
+			undo = {
+				hl_color = { bg = "#693232" }, -- Dark muted red
 			},
-			{
-				"U",
-				function()
-					require("undo-glow").redo({
-						animation = {
-							animation_type = "fade",
-						},
-					})
-				end,
-				mode = "n",
-				desc = "Redo with highlight",
-				noremap = true,
+			redo = {
+				hl_color = { bg = "#2F4640" }, -- Dark muted green
 			},
-			{
-				"gc",
-				function()
-					-- This is an implementation to preserve the cursor position
-					local pos = vim.fn.getpos(".")
-					vim.schedule(function()
-						vim.fn.setpos(".", pos)
-					end)
-					return require("undo-glow").comment()
-				end,
-				mode = { "n", "x" },
-				desc = "Toggle comment with highlight",
-				expr = true,
-				noremap = true,
+			yank = {
+				hl_color = { bg = "#7A683A" }, -- Dark muted yellow
 			},
-			{
-				"gc",
-				function()
-					require("undo-glow").comment_textobject()
-				end,
-				mode = "o",
-				desc = "Comment textobject with highlight",
-				noremap = true,
+			paste = {
+				hl_color = { bg = "#325B5B" }, -- Dark muted cyan
 			},
-			{
-				"gcc",
-				function()
-					return require("undo-glow").comment_line()
-				end,
-				mode = "n",
-				desc = "Toggle comment line with highlight",
-				expr = true,
-				noremap = true,
+			comment = {
+				hl_color = { bg = "#7A5A3D" }, -- Dark muted orange
+			},
+			cursor = {
+				hl_color = { bg = "#793D54" }, -- Dark muted pink
 			},
 		},
-		init = function()
-			vim.api.nvim_create_autocmd("TextYankPost", {
-				desc = "Highlight when yanking (copying) text",
-				callback = function()
-					require("undo-glow").yank()
-				end,
-			})
-
-			-- This only handles neovim instance and do not highlight when switching panes in tmux
-			vim.api.nvim_create_autocmd("CursorMoved", {
-				desc = "Highlight when cursor moved significantly",
-				callback = function()
-					require("undo-glow").cursor_moved({
-						animation = {
-							animation_type = "slide",
-						},
-					})
-				end,
-			})
-
-			vim.api.nvim_create_autocmd("CmdLineLeave", {
-				pattern = { "/", "?" },
-				desc = "Highlight when search cmdline leave",
-				callback = function()
-					require("undo-glow").search_cmd({
-						animation = {
-							animation_type = "fade",
-						},
-					})
-				end,
-			})
-			vim.keymap.set("n", "p", function()
-				return require("undo-glow").yanky_put("YankyPutAfter")
-			end, { noremap = true, desc = "Paste below with highlight", expr = true })
-
-			vim.keymap.set("n", "P", function()
-				return require("undo-glow").yanky_put("YankyPutBefore")
-			end, { noremap = true, desc = "Paste above with highlight", expr = true })
-		end,
+		priority = 2048 * 3,
 	},
-	{
-		"gbprod/yanky.nvim",
-		opts = {
-			system_clipboard = {
-				sync_with_ring = false, -- Don't sync with system clipboard automatically
-			},
-			highlight = {
-				on_put = false,
-				on_yank = false,
-			},
+	keys = {
+		{
+			"u",
+			function()
+				require("undo-glow").undo({
+					animation = {
+						animation_type = "fade",
+					},
+				})
+			end,
+			mode = "n",
+			desc = "Undo with highlight",
+			noremap = true,
 		},
-	}
+		{
+			"U",
+			function()
+				require("undo-glow").redo({
+					animation = {
+						animation_type = "fade",
+					},
+				})
+			end,
+			mode = "n",
+			desc = "Redo with highlight",
+			noremap = true,
+		},
+		{
+			"gc",
+			function()
+				-- This is an implementation to preserve the cursor position
+				local pos = vim.fn.getpos(".")
+				vim.schedule(function()
+					vim.fn.setpos(".", pos)
+				end)
+				return require("undo-glow").comment()
+			end,
+			mode = { "n", "x" },
+			desc = "Toggle comment with highlight",
+			expr = true,
+			noremap = true,
+		},
+		{
+			"gc",
+			function()
+				require("undo-glow").comment_textobject()
+			end,
+			mode = "o",
+			desc = "Comment textobject with highlight",
+			noremap = true,
+		},
+		{
+			"gcc",
+			function()
+				return require("undo-glow").comment_line()
+			end,
+			mode = "n",
+			desc = "Toggle comment line with highlight",
+			expr = true,
+			noremap = true,
+		},
+		{
+			"p",
+			function()
+				require("undo-glow").highlight_changes({ hlgroup = "UgPaste" })
+				vim.cmd("normal! p")
+			end,
+			mode = "n",
+			desc = "Paste below with highlight",
+			noremap = true,
+		},
+		{
+			"P",
+			function()
+				require("undo-glow").highlight_changes({ hlgroup = "UgPaste" })
+				vim.cmd("normal! P")
+			end,
+			mode = "n",
+			desc = "Paste above with highlight",
+			noremap = true,
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("TextYankPost", {
+			desc = "Highlight when yanking (copying) text",
+			callback = function()
+				require("undo-glow").yank()
+			end,
+		})
+
+		-- This only handles neovim instance and do not highlight when switching panes in tmux
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			desc = "Highlight when cursor moved significantly",
+			callback = function()
+				require("undo-glow").cursor_moved({
+					animation = {
+						animation_type = "slide",
+					},
+				})
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("CmdLineLeave", {
+			pattern = { "/", "?" },
+			desc = "Highlight when search cmdline leave",
+			callback = function()
+				require("undo-glow").search_cmd({
+					animation = {
+						animation_type = "fade",
+					},
+				})
+			end,
+		})
+	end,
 }
