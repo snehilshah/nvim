c5ad114250e3fdce7b3e0bb00782812ee0ce5c8b
snehilshah <snehilshah27@gmail.com>
2025-12-18 00:36:58 +0530

    better inline diagnostics

--- lua/plugins/tiny-diagnostic.lua
+++ lua/plugins/tiny-diagnostic.lua
@@ -8,10 +8,10 @@ return {
 			-- Available options:
 			-- "modern", "classic", "minimal", "powerline",
 			-- "ghost", "simple", "nonerdfont", "amongus"
-			preset = "powerline",
+			preset = "modern",
 
 			transparent_bg = false, -- Set the background of the diagnostic to transparent
-			transparent_cursorline = true, -- Set the background of the cursorline to transparent (only one the first diagnostic)
+			transparent_cursorline = false, -- Set the background of the cursorline to transparent (only one the first diagnostic)
 
 			hi = {
 				error = "DiagnosticError", -- Highlight group for error messages
@@ -32,7 +32,7 @@ return {
 				set_arrow_to_diag_color = true,
 				add_messages = {
 					messages = true, -- Show full diagnostic messages
-					display_count = false, -- Show diagnostic count instead of messages when cursor not on line
+					display_count = true, -- Show diagnostic count instead of messages when cursor not on line
 					use_max_severity = false, -- When counting, only show the most severe diagnostic
 					show_multiple_glyphs = true, -- Show multiple icons for multiple diagnostics of same severity
 				},
@@ -50,8 +50,8 @@ return {
 					enabled = true, -- Enable displaying related diagnostics
 					max_count = 3, -- Maximum number of related diagnostics to show per diagnostic
 				},
-				enable_on_insert = false,
-				enable_on_select = false,
+				enable_on_insert = true,
+				enable_on_select = true,
 
 				overflow = {
 					mode = "wrap",
