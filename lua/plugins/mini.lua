b868e682d66bca82e708d25a3e2794cab474d0b8
snehilshah <snehilshah27@gmail.com>
2025-12-28 06:45:38 +0530

    refine existing plugin configurations

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -32,6 +32,10 @@ return {
 		})
 		MiniIcons.mock_nvim_web_devicons()
 		require("mini.operators").setup()
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 	end,
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -77,6 +77,14 @@ return {
 			max_count = 20,
 			disable_mouse = false,
 			restriction_mode = "hint",
+			disabled_keys = {
+				["<Up>"] = { "", "i" },
+				["<Down>"] = { "", "i" },
+				-- left and right are left enabled for navigating buffers see keymaps.lua
+				-- anyways this keymap will not do what you are expecting to block here
+				["<Left>"] = {},
+				["<Right>"] = {},
+			},
 		},
 	},
 	-- just comfy line numbers
