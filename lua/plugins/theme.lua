face95af77c2601dd6b9dc6a5824b7bd8fb6642f
snehilshah <snehilshah27@gmail.com>
2026-01-01 04:37:16 +0000

    experimenting with new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,5 +2,7 @@ return {
 	"snehilshah/cosmic-gleam.nvim",
 	init = function()
 		vim.cmd.colorscheme("cosmic_gleam")
+		vim.g.colors_name = "cosmic-gleam.nvim"
+		vim.g.colors_name = "cosmic-gleam"
 	end,
 }
