ec3ff74e5170c7a314e9cca948e16994c6e3c544
snehilshah <snehilshah27@gmail.com>
2026-01-01 05:30:16 +0000

    fold and unfold with the same key

--- lua/plugins/languages/lessgo.lua
+++ lua/plugins/languages/lessgo.lua
@@ -65,7 +65,7 @@ return {
         enter = "<M-l>",
         -- when pressing the enter key, also look for the next concealed block below the cursor
         -- set to false to only enter a block that starts at/above the current line
-        refold = "<M-->",
+        refold = "<M-l>",
       },
     },
   },
