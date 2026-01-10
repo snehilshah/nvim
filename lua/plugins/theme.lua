1035bf51f19d7a5d7b04bc659e5a7ac974d2f649
snehilshah <snehilshah27@gmail.com>
2026-01-10 08:50:47 +0530

    playing around with theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,20 @@
 return {
-  "sainnhe/gruvbox-material",
-  init = function()
-    vim.cmd.colorscheme("gruvbox-material")
-    vim.g.colors_name = "gruvbox-material"
-  end,
+  {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    init = function()
+      vim.cmd.colorscheme("gruvbox-material")
+      vim.g.colors_name = "gruvbox-material"
+    end,
+  },
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    enabled = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- require("koda").setup({ transparent = true })
+      vim.cmd("colorscheme koda")
+    end,
+  },
 }
