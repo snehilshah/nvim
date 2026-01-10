76085cb1782ce70f970d94f332dcfc068787d64d
snehilshah <snehilshah27@gmail.com>
2026-01-10 08:50:21 +0530

    disable copilot and bruno.nvim by default

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -2,7 +2,7 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
-  enabled = true,
+  enabled = false,
   dependencies = {
     "copilotlsp-nvim/copilot-lsp",
   },
