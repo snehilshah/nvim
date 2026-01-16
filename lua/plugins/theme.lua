0db2d71a65b981ab50cb248acd4d57af2750fc9b
snehilshah <snehilshah27@gmail.com>
2026-01-16 12:20:59 +0530

    cleanup redundant theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,14 +7,4 @@ return {
       vim.g.colors_name = "gruvbox-material"
     end,
   },
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    enabled = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- require("koda").setup({ transparent = true })
-      vim.cmd("colorscheme koda")
-    end,
-  },
 }
