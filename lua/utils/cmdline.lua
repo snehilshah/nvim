1202b18895d45d622b8df9e4c35e69b147f102a4
snehilshah <snehilshah27@gmail.com>
2026-02-01 02:23:19 +0530

    chore: maintenance and documentation of utility modules

--- lua/utils/cmdline.lua
+++ lua/utils/cmdline.lua
@@ -1,3 +1,11 @@
+-- ============================================================================
+-- EXPERIMENTAL: Dynamic Command Line Height
+-- ============================================================================
+-- This module dynamically adjusts cmdheight to show cmdline with statusbar.
+-- Currently disabled in init.lua - enable with: require('utils.cmdline')
+-- May conflict with noice.nvim or other cmdline UI plugins.
+-- ============================================================================
+
 -- dynamically change the height of the command line so that we can see the cmdline with the status bar
 local cmdGrp = vim.api.nvim_create_augroup("cmdline_height", { clear = true })
 local function set_cmdheight(val)
