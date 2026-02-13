cd5cd6def076400f085704c7fbe4d7d398ccb148
snehilshah <snehilshah27@gmail.com>
2026-02-14 01:18:14 +0530

    refactor(lsp): disable markdown_oxide and prune lsp library paths

--- after/lsp/markdown_oxide.lua
+++ after/lsp/markdown_oxide.lua
@@ -1,45 +1,48 @@
-return {
-  capabilities = {
-    workspace = {
-      didChangeWatchedFiles = {
-        dynamicRegistration = true,
-      },
-    },
-  },
-  on_attach = function(client, bufnr)
-    -- Codelens support
-    local function check_codelens_support()
-      if client.server_capabilities.codeLensProvider then
-        return true
-      end
-      return false
-    end
+-- TODO: Re-enable markdown_oxide LSP config when needed
+-- return {
+--   capabilities = {
+--     workspace = {
+--       didChangeWatchedFiles = {
+--         dynamicRegistration = true,
+--       },
+--     },
+--   },
+--   on_attach = function(client, bufnr)
+--     -- Codelens support
+--     local function check_codelens_support()
+--       if client.server_capabilities.codeLensProvider then
+--         return true
+--       end
+--       return false
+--     end
+--
+--     vim.api.nvim_create_autocmd(
+--       { "InsertLeave", "CursorHold", "LspAttach", "BufEnter" },
+--       {
+--         buffer = bufnr,
+--         callback = function()
+--           if check_codelens_support() then
+--             vim.lsp.codelens.refresh({ bufnr = bufnr })
+--           end
+--         end,
+--       }
+--     )
+--
+--     -- Initial codelens refresh
+--     if check_codelens_support() then
+--       vim.lsp.codelens.refresh({ bufnr = bufnr })
+--     end
+--
+--     -- Daily note command
+--     vim.api.nvim_create_user_command("Daily", function(args)
+--       local input = args.args
+--       if input == "" then
+--         client:exec_cmd({ command = "jump", arguments = {} })
+--       else
+--         client:exec_cmd({ command = "jump", arguments = { input } })
+--       end
+--     end, { desc = "Open daily note", nargs = "*" })
+--   end,
+-- }
 
-    vim.api.nvim_create_autocmd(
-      { "TextChanged", "InsertLeave", "CursorHold", "LspAttach", "BufEnter" },
-      {
-        buffer = bufnr,
-        callback = function()
-          if check_codelens_support() then
-            vim.lsp.codelens.refresh({ bufnr = bufnr })
-          end
-        end,
-      }
-    )
-
-    -- Initial codelens refresh
-    if check_codelens_support() then
-      vim.lsp.codelens.refresh({ bufnr = bufnr })
-    end
-
-    -- Daily note command
-    vim.api.nvim_create_user_command("Daily", function(args)
-      local input = args.args
-      if input == "" then
-        client:exec_cmd({ command = "jump", arguments = {} })
-      else
-        client:exec_cmd({ command = "jump", arguments = { input } })
-      end
-    end, { desc = "Open daily note", nargs = "*" })
-  end,
-}
+return {}
