ba848c26666d35e0cf4c06835f9584e1b2021c48
snehilshah <snehilshah27@gmail.com>
2026-02-15 02:36:24 +0530

    chore(which-key): set popup time to 1000ms

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,9 +9,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      delay = 1000, -- ms before popup shows (default: 200)
     },
     keys = {
       {
