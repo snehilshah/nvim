4cbac7531a0c93f34db17b7c325f260d606dc1cb
snehilshah <snehilshah27@gmail.com>
2026-02-26 17:39:40 +0530

    feat(lsp): restrict angularls to projects with angular configuration

--- after/lsp/angularls.lua
+++ after/lsp/angularls.lua
@@ -1,3 +1,15 @@
 return {
   root_markers = { "angular.json", "project.json" },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local angular_json = vim.fs.find({ "angular.json", "project.json" }, {
+      path = fname,
+      upward = true,
+      type = "file",
+    })[1]
+    if angular_json then
+      on_dir(vim.fs.dirname(angular_json))
+    end
+    -- Return nothing to prevent angularls from starting in non-Angular projects
+  end,
 }
