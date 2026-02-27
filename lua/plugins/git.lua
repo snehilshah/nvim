e1f373440498e897bc1ac00c8235209e44b48c06
snehilshah <snehilshah27@gmail.com>
2026-02-27 17:24:45 +0530

    imp(gitlineage): just directly write commands no need all keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -272,6 +272,16 @@ return {
   },
   {
     "lionyxml/gitlineage.nvim",
-    opts = {},
+    opts = {
+      split = "auto", -- "vertical", "horizontal", or "auto"
+      -- keymap = "<leader>gl", -- just use the command `:GitLineage`
+      keys = {
+        close = "q", -- set to nil to disable
+        next_commit = "]c", -- set to nil to disable
+        prev_commit = "[c", -- set to nil to disable
+        yank_commit = "yc", -- set to nil to disable
+        open_diff = "<CR>", -- set to nil to disable (requires diffview.nvim)
+      },
+    },
   },
 }
