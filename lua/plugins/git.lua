2ea7973db8b20c0b933caf85cd5155e184d1bd6a
snehilshah <snehilshah27@gmail.com>
2026-02-28 02:24:27 +0530

    plugin(lineage): drop gitlineage as codediff can do that now

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -270,18 +270,4 @@ return {
     cmd = "Atone",
     opts = {},
   },
-  {
-    "lionyxml/gitlineage.nvim",
-    opts = {
-      split = "auto", -- "vertical", "horizontal", or "auto"
-      -- keymap = "<leader>gl", -- just use the command `:GitLineage`
-      keys = {
-        close = "q", -- set to nil to disable
-        next_commit = "]c", -- set to nil to disable
-        prev_commit = "[c", -- set to nil to disable
-        yank_commit = "yc", -- set to nil to disable
-        open_diff = "<CR>", -- set to nil to disable (requires diffview.nvim)
-      },
-    },
-  },
 }
