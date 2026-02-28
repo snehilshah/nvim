017625bc9460f3e48179d8c74fca494dfe0940d8
snehilshah <snehilshah27@gmail.com>
2026-03-01 05:04:40 +0530

    chore(deps): upgrade mini.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,6 @@ return {
   lazy = false, -- Load immediately, not lazily
   version = false,
   config = function()
-    require("mini.ai").setup({ n_lines = 500 }) -- this is not a "AI" plugin
     require("mini.cmdline").setup()
     require("mini.cursorword").setup()
     require("mini.icons").setup({
