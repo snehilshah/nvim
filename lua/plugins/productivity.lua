17f233d8c3b80691d1128160194fe80fe6553820
snehilshah <snehilshah27@gmail.com>
2026-03-03 10:25:08 +0530

    feat(norm): new learning file with norm command learning plugin

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -4,25 +4,6 @@
 -- Habits, timers, clipboard management, and TODO tracking
 
 return {
-  -- Encourages best practices and breaks bad habits
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
-      max_count = 20,
-      disable_mouse = false,
-      restriction_mode = "hint",
-      disabled_keys = {
-        ["<Up>"] = { "", "i" },
-        ["<Down>"] = { "", "i" },
-        -- left and right are left enabled for navigating buffers see keymaps.lua
-        -- anyways this keymap will not do what you are expecting to block here
-        ["<Left>"] = {},
-        ["<Right>"] = {},
-      },
-    },
-  },
   -- Simple timer plugin for Neovim
   {
     "nvzone/timerly",
