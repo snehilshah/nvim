00880b6ad706a27751bde552107221f28837a66a
snehilshah <snehilshah27@gmail.com>
2026-03-11 00:22:03 +0530

    keymaps(todo): snacks picker for todo list

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -61,6 +61,20 @@ return {
         end,
         desc = "Previous todo comment",
       },
+      {
+        "<leader>st",
+        function()
+          Snacks.picker.todo_comments()
+        end,
+        desc = "Todo",
+      },
+      {
+        "<leader>sT",
+        function()
+          Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+        end,
+        desc = "Todo/Fix/Fixme",
+      },
     },
     event = "VeryLazy",
     opts = {
