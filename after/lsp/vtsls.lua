2ea9060aaebb009d6f16c8acba0f3ca94965e43c
snehilshah <snehilshah27@gmail.com>
2026-03-14 15:59:25 +0530

    feat(lsp): add few types to lsp server configs

--- after/lsp/vtsls.lua
+++ after/lsp/vtsls.lua
@@ -1,5 +1,7 @@
 -- vtsls configuration (using native VS Code-style typescript settings)
+---@type vim.lsp.Config
 return {
+  ---@type lspconfig.settings.vtsls
   settings = {
     vtsls = {
       enableMoveToFileCodeAction = true,
