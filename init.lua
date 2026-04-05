7f54ee2b005736c02a592cb197dd99f1536030b0
snehilshah <snehilshah27@gmail.com>
2026-04-05 08:45:22 +0530

    feat(ui2): enable ui2

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
+
+-- enable ui2
+vim.g.loaded_ui2 = true
+require("vim._core.ui2").enable({
+  enable = true,
+  msg = {
+    target = "msg",
+  }
+})
+
 -- disable netrw
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
