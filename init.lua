1c0405f6d1c6bd7155bc64b43b4e0ee0089e9a1e
snehilshah <snehilshah27@gmail.com>
2026-04-11 02:51:25 +0530

    chore(ui2): disable ui2

--- init.lua
+++ init.lua
@@ -4,15 +4,6 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
--- enable ui2
-vim.g.loaded_ui2 = true
-require("vim._core.ui2").enable({
-  enable = true,
-  msg = {
-    target = "msg",
-  }
-})
-
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
