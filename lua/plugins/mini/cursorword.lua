4fc94642aac8232e63f19d09e20fd15dd91b2828
snehilshah <snehilshah27@gmail.com>
2026-04-28 11:57:06 +0530

    refactor(mini): modularize mini.nvim plugins into individual files

---
+++ lua/plugins/mini/cursorword.lua
@@ -0,0 +1,5 @@
+return {
+    "nvim-mini/mini.cursorword",
+    event = "VeryLazy",
+    opts = {},
+}
---
+++ lua/plugins/mini/surround.lua
@@ -0,0 +1,9 @@
+-- Add/delete/replace surroundings (brackets, quotes, etc.)
+-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+-- - sd'   - [S]urround [D]elete [']quotes
+-- - sr)'  - [S]urround [R]eplace [)] [']
+return {
+    "nvim-mini/mini.surround",
+    event = "VeryLazy",
+    opts = {},
+}
