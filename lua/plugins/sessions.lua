75a5c7811be4c167e11878518e5f834fcc6b2ef3
snehilshah <snehilshah27@gmail.com>
2026-04-30 03:02:03 +0530

    feat(sessions): add keymaps for session management

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,5 +1,21 @@
 return {
     "folke/persistence.nvim",
     event = "BufReadPre",
+    keys = {
+        {
+            "<leader>qs",
+            function()
+                require("persistence").load()
+            end,
+            desc = "Restore session (cwd)",
+        },
+        {
+            "<leader>qS",
+            function()
+                require("persistence").select()
+            end,
+            desc = "Select a session",
+        },
+    },
     opts = {},
 }
