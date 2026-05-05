947147dc112258e4165588572e862eccb18f1e96
snehilshah <snehilshah27@gmail.com>
2026-05-05 11:32:35 +0530

    fix(schemastore): remove unnecessary module specification for SchemaStore.nvim

--- after/lsp/vimdoc_ls.lua
+++ after/lsp/vimdoc_ls.lua
@@ -1,8 +1,8 @@
 --- Install with: cargo install vimdoc-language-server
 ---@type vim.lsp.Config
 return {
-    cmd = { 'vimdoc-language-server' },
-    filetypes = { 'help' },
-    root_markers = { 'doc', '.git' },
+    cmd = { "vimdoc-language-server" },
+    filetypes = { "help" },
+    root_markers = { "doc", ".git" },
     workspace_required = false,
 }
